be755d6ea16474afcac67ec44bc1aac867b6f719
midelus <penstria2024@gmail.com>
2026-04-20 22:08:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title(body, anchor=None, *, help=None, width="stretch", text_alignment="left")
+
 st.title("Raphael Budiputra")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
